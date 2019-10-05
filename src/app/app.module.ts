8bd5fd7add304494a082b29fc8600aa8efd5e11a
Christian <christiangerardhizon@gmail.com>
2019-10-05 15:40:34 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; // import this.
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,16 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule, // <----- Add this to imports
+    IonicModule.forRoot(),
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  url: string = null;
 
+  users: any = [];
+
+  name: string;
+  lastname: string;
+  birthday: string;
+  image: string;
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  // Fetch all users
+  fetch() {
+    // Basic HTTP Request Sample
+    this.http.get('http://192.168.137.1/ionic-php/user.php', {
+      observe: 'response',
+    })
+      .subscribe((data: any) => {
+        console.log(data);
+        this.users = data.users;
+      });
+  }
+
+  // Create New User
+  newUser() {
+
+    // Request Payload
+    const payload = {
+      name: this.name,
+      lastname: this.lastname,
+      birthday: this.birthday,
+      image: this.image,
+    };
+
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+
+    this.http.post('http://192.168.137.1/ionic-php/user.php', payload, {
+      headers: headers,
+    }).subscribe(response => {
+      alert(response);
+      this.fetch();
+    });
+
+  }
+
+  // Barcode Scanner
+  async scan() {
+    // try {
+    //   const res = await this.barcodeScanner.scan();
+    //   this.url = res.text;
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  }
 }
